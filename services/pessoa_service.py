6d889f535e568a0bfbf5209367336be8f33323ee
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-03 20:02:37 -0300

    Criado e implementado a codificação pessoa_service.py

---
+++ services/pessoa_service.py
@@ -0,0 +1,25 @@
+from repositories.pessoa_repository import PessoaRepository
+
+class PessoaService:
+    #método contrutor
+    def __init__(self):
+        self.repo = PessoaRepository()
+
+    #serviço listar
+    def listar(self, db):
+        return self.repo.listar(db)
+    
+    #serviço cadastrar
+    def cadastrar(self, db, pessoa):
+        return self.repo.cadastar(db, pessoa)
+
+    # serviço alterar
+    def alterar(self, db, id, pessoa):
+        return self.repo.alterar(db, id, pessoa)
+
+
+    # serviço excluir
+    def excluir(self, db, id):
+        return self.repo.excluir(db, id)
+
+    #faltam alterar e excluir
