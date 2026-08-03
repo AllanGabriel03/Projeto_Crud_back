51593caec0cd636ac98cf5e4d56308741d4da36a
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-03 20:00:43 -0300

    Criado e implementado a codificação pessoa_model.py

---
+++ models/pessoa_model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DECIMAL, Date
+
+from database import Base
+
+class Pessoa(Base):
+    __tablename__ = "pessoa"
+    
+    idpessoa = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(60))
+    sexo = Column(String(1))
+    datanascimento = Column(Date)
+    peso = Column(Integer)
+    altura = Column(DECIMAL(10,2))
+    
+
