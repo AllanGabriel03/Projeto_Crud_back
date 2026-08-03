f251d9b0536c10e3f8b05255dc741bc46e3a8c72
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-03 20:00:14 -0300

    Criado e implementado a codificação pessoa_controller.py

---
+++ controllers/pessoa_controller.py
@@ -0,0 +1,23 @@
+from services.pessoa_service import PessoaService
+
+class PessoaController:
+    #método construtor
+    def __init__(self):
+        self.servico = PessoaService()
+    
+    #controler listar
+    def listar(self, db):
+        return self.servico.listar(db)
+    
+    #controle cadastrar
+    def cadastrar(self, db, pessoa):
+        return self.servico.cadastrar(db, pessoa)
+
+  # controller alterar pessoa
+    def alterar(self, db, id, pessoa):
+        return self.servico.alterar(db, id, pessoa)
+
+
+    # controller excluir pessoa
+    def excluir(self, db, id):
+        return self.servico.excluir(db, id)
