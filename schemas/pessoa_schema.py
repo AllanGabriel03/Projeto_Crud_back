b47c0c58bb88ae2e0f394ad639969336eb65844c
WallaceOliveiradosSantos <wallace@docente.senai.br>
2026-08-26 20:36:48 -0300

    Adicionado novos atributos

--- schemas/pessoa_schema.py
+++ schemas/pessoa_schema.py
@@ -3,7 +3,14 @@ from datetime import date
 
 class PessoaSchema(BaseModel):
     nome : str
-    sexo : str
-    datanascimento: date
+    cpf: int
+    data_nascimento: date
     peso: int
     altura: float
+    sexo : str
+    cep : int
+    rua_logradouro : str
+    bairro : str
+    cidade : str
+    uf : str
+    
